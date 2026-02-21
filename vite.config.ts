100d5a672e46c379d337c360fb9712508e3dcae3
rekasapriana <rekas.apriana@gmail.com>
2026-02-21 07:12:02 +0700

    Fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/kanban/',
+  base: '/kanban.github.io/',
   build: {
     outDir: 'dist',
     emptyDirBeforeWrite: true
